4b21ffc2d38e9dae564786f0444943ac6af63225
AlvaroTrabajo1999 <AlvaroTrabajo1999@gmail.com>
2020-04-28 23:16:20 +0200

    PArticipante Nuevo

--- src/modelo/dao/BaseDao.java
+++ src/modelo/dao/BaseDao.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSession;
 
 import modelo.dao.mapper.BaseMapper;
 import modelo.pojo.Motocicleta;
+import modelo.pojo.Participacion;
 import modelo.pojo.Piloto;
 import modelo.pojo.Torneo;
 
@@ -31,6 +32,16 @@ public class BaseDao {
 		}
 	}
 	
+	public Motocicleta getMotocicletaFromMatricula(String matricula){
+		SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+		try {
+			BaseMapper maper = sqlSession.getMapper(BaseMapper.class);
+			return maper.getMotocicletaFromMatricula(matricula);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
 	public ArrayList<Piloto> getPilotos(){
 		SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
 		try {
@@ -41,4 +52,24 @@ public class BaseDao {
 		}
 	}
 	
+	public Piloto getPilotoFromDni(String dni){
+		SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+		try {
+			BaseMapper maper = sqlSession.getMapper(BaseMapper.class);
+			return maper.getPilotoFromDni(dni);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public void insertParticipante(Participacion part) {
+		SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+		try {
+			BaseMapper maper = sqlSession.getMapper(BaseMapper.class);
+			maper.insertParticipante(part);
+			sqlSession.commit();
+		} finally {
+			sqlSession.close();
+		}
+	}
 }
